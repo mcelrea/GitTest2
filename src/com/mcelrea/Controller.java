37d271b7ca7a6225816f714fa688e2ce54878bd6
Eric McElrea <mcelrea@damien-hs.edu>
2018-08-02 08:44:34 -0700

    buttons working

--- src/com/mcelrea/Controller.java
+++ src/com/mcelrea/Controller.java
@@ -1,4 +1,21 @@
 package com.mcelrea;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+
+    @FXML
+    Button button1;
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    public void handleButtonPress(MouseEvent e) {
+        textArea.setText("Button pressed");
+    }
 }
--- src/com/mcelrea/Main.java
+++ src/com/mcelrea/Main.java
@@ -1,9 +1,19 @@
 package com.mcelrea;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +23,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
+
+
         primaryStage.show();
     }
 
